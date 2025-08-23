29b4d26524b40f1c190cb04c5fa937e5ee8ca963
Abdullah Al Sazib <mdsazibhossin2021@gmail.com>
2025-08-23 17:22:24 +0600

    add new section

---
+++ inc_dec/problem/problem3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 1;
+    int b = 2;
+
+    if (a-- > 0 || ++b > 2)
+    {
+        cout << "Stage1 - Inside if";
+    }
+    else
+    {
+        cout << "Stage2 = Inside else"; // stage2 - inside else
+    }
+    cout << endl << a << " " << b << endl; // a = 0 and b = 3
+
+    return 0;
+}
