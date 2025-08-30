d57ceb8baedd36ccfc8d31220b877f8eeb83db56
abdullahalsazib <mdsazibhossin2021@gmail.com>
2025-08-30 23:07:42 +0600

    add new more chapter+

---
+++ dsa-session/chapter-11/sorting-algo/bubble.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <utility>
+using namespace std;
+
+void printArray(int arr[], int n) {
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+}
+void bubbleSort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
+      }
+    }
+  }
+  printArray(arr, n);
+}
+
+int main() {
+  cout << "Hello World" << endl;
+  int arr[] = {5, 4, 1, 3, 2};
+  int n = (sizeof(arr) / sizeof(int));
+
+  // call the bubble sort function
+  bubbleSort(arr, n);
+  return 0;
+}
